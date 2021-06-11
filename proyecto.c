e82bd273dea3a3d0993c66d8054201bee3b48e43
guerrapel02 <paulina.guerra4659@alumnos.udg.mx>
2021-06-11 02:38:04 -0500

    cambio

--- proyecto.c
+++ proyecto.c
@@ -1,100 +1,161 @@
-/*      SEMINARIO DE SOLUCIÓN DE 
+/*      SEMINARIO DE SOLUCIÓN DE
         PROBLEMAS DE PREGRAMACIÓN:
-                PROYECTO 
-    
-    EQUIPO 2: 
-    GUERRA LÓPEZ PAULINA ESTEFANIA 
+                PROYECTO
+
+    EQUIPO 2:
+    GUERRA LÓPEZ PAULINA ESTEFANIA
     MOISES
 
     TEMA: SISTEMA MEDICO ADMINISTRATIVO
             *CLIMEF SOFT*   */
 
+//  librerias
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <conio.h>
 
-#define "ADMIN"
-#define "123"
+//  Constantes
+#define USUARIO "ADMIN"
+#define CLAVE "123"
+#define TECLA_ENTER 13
+#define TECLA_BACKSPACE 8
+#define LONGITUD 5
+#define MAX_INTENTOS 3
 
-//  Registro del sistema
-struct agenda
-{
-    char empleado[50], consulta[50];
-    char paciente[50], nombre[50], edad[50], sexo[50], telefono[50];
-    char rfc[50], tipo_paciente[50], curp[50], domicilio[50];
-};sistema[700];
+//  Prototipos
+void menu_principal();
+void submenu():
 
-// menu principal
-char menu_principal()
-{   //  se muestran las opciones
-    printf("\n\n\tMENU PRINCIPAL");
-    printf("\n\t1)  Empleado ");
-    printf("\n\t2)  Paciente ");
-    printf("\n\t3)  Consulta ");
-    printf("\n\t0)  Salir ");
+//  Estructuras
+    //  Las estructuras no estan completas me revolivi en este apartado
+    //  empleadp
+struct empleado{
+    int num_empleado;
+    char nombre[30];
 };
 
-void main()
-{
-    int seguir = 1, opc_1;
-    char usuario, contraseña;
+    //  Paciente
+struct paciente{
+    char nombre [30];
+    int edad, rfc;
+    char curp[30], domicilio[20], telefono[10];
+};
 
-    while seguir == 1   //  entra a un ciclo el cual no sabemos cuando va a parar
-    {
-        system("cls");  //  limpia pantalla
-        printf("\n\tUsuario: ");    //  pide auntentificación
-        fflush(stdin)
-        scanf("%[^\n]s", &usuario);
-        printf("\n\tContraseña: ");
-        scanf("%[^\n]s", &contraseña);
+    // consulta
+struct consulta{
+    char id[10],
+    int rfcc;
+    char nombre[30];
+};
 
-        if (usuario == "ADMIN" & contraseña == "123")   //  si la la autentificación es correcta
-        {
-            system("cls");
-            menu_principal();
-            printf("\n\tElige una opción: ");
-            scanf("%d", &opc_1);
-            if opc >=1 & opc <=3
-            {
-                switch (opc)
-                {
-                    case 1:
-                        empleado_sub();
-                        break;
-                    case 2:
-                        paciente_sub();
-                        break;
-                    case 3:
-                        consulta_sub();
-                }
-            }
-            else if opc == 0
+
+// main
+int main() {
+    //  variables
+	char usuario[LONGITUD + 1];
+	char clave[LONGITUD + 1];
+	int intento = 0;
+	int ingresa = 0;
+	char caracter;
+	int i = 0, opc, seguir = 1;
+
+	do {    //  ciclo para el ingreso
+		i = 0;
+		system("cls");
+		printf("\n\t\t\tINICIO DE SESION\n");
+		printf("\n\t\tUSUARIO: ");
+		gets(usuario);
+		printf("\t\tCONTRASEÑA: ");
+		while (caracter = getch()) {
+			if (caracter == TECLA_ENTER) {
+				clave[i] = '\0';
+				break;
+
+			} else if (caracter == TECLA_BACKSPACE) {
+				if (i > 0) {
+					i--;
+					printf("\b \b");
+				}
+
+			} else {
+				if (i < LONGITUD) {
+					printf("*");
+					clave[i] = caracter;
+					i++;
+				}
+			}
+		}
+
+		if (strcmp(usuario, USUARIO) == 0 && strcmp(clave, CLAVE) == 0) {
+			ingresa = 1;
+
+		} else {
+			printf("\n\t\tUsuario y/o clave son incorrectos\n");
+			intento++;
+			getchar();
+		}
+
+	} while (intento < MAX_INTENTOS && ingresa == 0);
+
+	while (seguir == 1){
+        if (ingresa == 1) {
+        system("cls");
+		printf("\n\n\t\tBienvenido al Sistema\n");
+		menu_principal();
+		printf("\n\t\tSeleciona una opcion: ");
+		scanf("%d", &opc);
+
+		switch (opc){
+            case 1:
+                system("cls");
+                printf("\n\n\t\tEmpleado\n");
+                submenu();
+                break:
+            case 2:
+                system("cls");
+                printf("\n\n\t\tPaciente\n");
+                submenu();
+                break:
+            caso 3:
+                system("cls");
+                printf("\n\n\t\tConsulta\n");
+                submenu();
+                break:
+            caso 0:
                 seguir = 0;
-            else
-                printf("\n\tHas ingresado una opción incorrecta, vuelve a intentar")
+                break:
+            default:
+                system("cls");
+                printf("\n\n\tHa ingresado una opcion incorrecta, intente de nuevo\n");
+
+		}
+
+
+
+        }
+        else {
+            system("cls");
+            printf("\n\n\tHa sobrepasado el numero maximo de intentos permitidos\n");
         }
 
-        else if (usuario == "ADMIN" & contraseña != "123")    //  si solo
-        {
-            printf("\n\tLa contraseña es incorrecta");
-        }
-        else if (contraseña == "123" & usuario != "ADMIN")
-        {
-            printf("\n\tEl usuario es incorrecta");
-        }
-        else
-            printf("\n\tUsuario y contraseña incorrectos");
-        
-    } 
+	}
 
+	return 0;
+}
+
+//  Menu
+void menu_principal(){
+    printf("\n\n\t\tMenu:");
+    printf("\n\t\t1) Empleado");
+    printf("\n\t\t2) Paciente");
+    printf("\n\t\t3) Consulta");
+    printf("\n\t\t0) Salir");
 };
 
-void empleado_sub()
-{
-    system("cls");
-    printf("\n\t1) Alta");
-    printf("\n\t2) Visualizar");
-    printf("\n\t3) Búsqueda");
-    printf("\n\t0) Regresar al menú anterior")
+//Submenu
+void submenu(){
+    printf("\n\t\t1) Alta (registro de datos)");
+    printf("\n\t\t2) Visualizar (mostrar datos)");
+    printf("\n\t\t3) Busqueda (localización de datos)");
+    printf("\n\t\t0) Regresar al menú anterior");
 };
-
